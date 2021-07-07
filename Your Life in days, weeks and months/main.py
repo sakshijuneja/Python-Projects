560ead9dc39349503876468ee4f65ec5c03c205c
Sakshi Juneja <77523667+sakshijuneja@users.noreply.github.com>
2021-07-08 02:37:20 +0530

    Your Life in days, weeks and months Program

---
+++ Your Life in days, weeks and months/main.py
@@ -0,0 +1,7 @@
+age = input("What is your current age? ")
+years_left=90-int(age)
+days_left=365*years_left
+weeks_left=52*years_left
+months_left=12*years_left
+
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left,if we live until 90 yrs old.")
